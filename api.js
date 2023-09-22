d2a147aa128bbf7581932ea6b0468acff5d3e192
Johan Kerry Lozano Ruiz <johanlozano4422@gmail.com>
2023-09-21 21:39:48 -0500

    Delete Task

--- api.js
+++ api.js
@@ -83,5 +83,15 @@ router.post('/update-task', function (req, res) {
     });
 });
 
+router.delete('/delete-task', function (req, res) {
+    TaskModel.deleteOne({ TaskId: req.body.TaskId }, function (err, data) {
+        if (err) {
+            res.status(500).send("Internal error\n");
+        } else {
+            res.status(200).send("OK\n");
+        }
+    });
+});
+
 module.exports = router;
 
